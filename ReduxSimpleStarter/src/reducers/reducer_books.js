3ab06a29a7bb3b27769a98d22c9ae398c165f99f
GaelLendormy <gael.lendormy@gmail.com>
2018-04-29 17:00:32 +0200

    first container

---
+++ ReduxSimpleStarter/src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function () {
+    return [
+        { title: 'Javascript:The good parts' },
+        { title: 'Harry Potter' },
+        { title: 'The dark Tower' },
+        { title: 'Eloquent Ruby' },
+        { title: 'Javascript Allongee'}
+    ]
+}
